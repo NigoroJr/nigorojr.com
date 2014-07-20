beba3608c05260bbd93027a190c387ea5ae22261
Naoki Mizuno <nigorojr@gmail.com>
2014-07-20 06:05:45 -0400

    Use proper hashed password

--- db/seeds/development/users.rb
+++ db/seeds/development/users.rb
@@ -1,19 +1,19 @@
 # coding: utf-8
 
 User.create(
-  screen: "Naoki",
-  username: "nigorojr",
-  hashed_password: "password",
+  screen: "NigoroJr",
+  username: "naoki",
+  hashed_password: "$2a$10$9sK8ojTvFTtfyxqgE/GW2.YAvTuxVc5Nuwbp3W4t9uFyRt8ucNz5q",
 )
 
 User.create(
   screen: "TestUser",
   username: "test",
-  hashed_password: "password2",
+  hashed_password: "$2a$10$9sK8ojTvFTtfyxqgE/GW2.YAvTuxVc5Nuwbp3W4t9uFyRt8ucNz5q",
 )
 
 User.create(
   screen: "Foobar",
   username: "foo",
-  hashed_password: "password3",
+  hashed_password: "$2a$10$9sK8ojTvFTtfyxqgE/GW2.YAvTuxVc5Nuwbp3W4t9uFyRt8ucNz5q",
 )
