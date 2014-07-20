39ed08789802099890d2ccd9775e7d39962c2e66
Naoki Mizuno <nigorojr@gmail.com>
2014-07-20 15:53:20 -0400

    Implement image upload

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -14,11 +14,30 @@ class PhotosController < ApplicationController
   end
 
   def create
-    attributes = Photo.permit(:photo).require(:location, :object, :category, :file_path)
+    attributes = params.require(:photo).permit(:location, :object, :category)
     @photo = Photo.new(attributes)
 
+    object = params[:photo][:object]
+    location = params[:photo][:location]
+
+    # Relative path of parent directory based on app/assets/images/
+    partial_path = "GT6"
+    # Write in RAILS_ROOT/app/assets/images/GT6
+    parent_dir = sprintf "%s/app/assets/images/%s", Rails.root, partial_path
+    # Build file name
+    file_name = get_image_file_name(parent_dir, object, location, "jpg")
+
+    # Write recieved binary data to file
+    image_binary = params[:photo][:image_data].read
+    file_path = sprintf "%s/%s", parent_dir, file_name
+    File.open(file_path, "wb").write(image_binary)
+
+    # file_path needs to be a relative path from app/assets/images/
+    @photo.file_path = sprintf "%s/%s", partial_path, file_name
+
     if @photo.save
-      redirect_to @photos
+      index
+      render "index"
     else
       render "new"
     end
@@ -31,5 +50,20 @@ class PhotosController < ApplicationController
   end
 
   def update
+  end
+
+  private
+  # Builds a valid file name from the given information
+  def get_image_file_name(parent_dir, object, location, extension)
+    index = 0
+    begin
+      file_name = sprintf "%s_%s_%03d.%s",
+        object.downcase.sub(" ", "-"),
+        location.downcase.sub(" ", "-"), index, extension
+      full_path = sprintf "%s/%s", parent_dir, file_name
+      index += 1
+    end while File.exists?(full_path)
+
+    return file_name
   end
 end
--- db/seeds/development/photos.rb
+++ db/seeds/development/photos.rb
@@ -4,26 +4,26 @@ Photo.create(
   location: "Trial Mountain",
   object: "Honda NSX Type-R;Nissan GT-R",
   category: "Race",
-  file_path: "gt6/test_image1.jpg",
+  file_path: "GT6/test_image1.jpg",
 )
 
 Photo.create(
   location: "Trial Mountain",
   object: "Nissan GT-R",
   category: "Time Trial",
-  file_path: "gt6/test_image2.jpg",
+  file_path: "GT6/test_image2.jpg",
 )
 
 Photo.create(
   location: "Tsukuba Circuit",
   object: "Mazda RX-7",
   category: "Drift",
-  file_path: "gt6/test_image3.jpg",
+  file_path: "GT6/test_image3.jpg",
 )
 
 Photo.create(
   location: "Autumn Ring Mini",
   object: "Chevrolet Corvette",
   category: "Drift",
-  file_path: "gt6/test_image4.jpg",
+  file_path: "GT6/test_image4.jpg",
 )
