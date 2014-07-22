6a7b16eeb57cd7ff5abf683256708ad9ddcc7f1a
Naoki Mizuno <nigorojr@gmail.com>
2014-07-22 01:01:47 -0400

    Add seed data for "root"

--- db/seeds/development/users.rb
+++ db/seeds/development/users.rb
@@ -17,3 +17,9 @@ User.create(
   username: "foo",
   hashed_password: "$2a$10$9sK8ojTvFTtfyxqgE/GW2.YAvTuxVc5Nuwbp3W4t9uFyRt8ucNz5q",
 )
+
+User.create(
+  screen: "Root",
+  username: "root",
+  hashed_password: "$2a$10$9sK8ojTvFTtfyxqgE/GW2.YAvTuxVc5Nuwbp3W4t9uFyRt8ucNz5q",
+)
