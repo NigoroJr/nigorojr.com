957f2901b4e5f472475e0630475e8752cd402882
Naoki Mizuno <nigorojr@gmail.com>
2014-07-21 21:14:18 -0400

    Fix bugs when validating input when creating user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessor :raw_password, :raw_password_confirmation
 
-  validates :raw_password, confirmation: true
-  validates :raw_password_confirmation, presence: true
-  validates :username, uniqueness: true
+  validates_uniqueness_of :username
 
   def raw_password=(val)
     if val.present?
--- db/migrate/20140719012315_create_users.rb
+++ db/migrate/20140719012315_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :screen
-      t.string :username, null: false, uniq: true;
+      t.string :username, null: false, unique: true
       t.string :hashed_password, null: false
 
       t.timestamps
