9e1f0f365351bc8fa10d600a234e90b39389c879
Naoki Mizuno <nigorojr@gmail.com>
2014-07-25 20:24:14 -0400

    Add validation for name for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :article, class_name: "Article", foreign_key: "article_id"
 
+  validates :name, presence: true
   validates :body, presence: true, length: { maximum: 2000 }
 end
