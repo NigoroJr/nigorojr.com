bec21db74c703e630d78918991f6c6a12a76fa3e
Naoki Mizuno <nigorojr@gmail.com>
2014-08-03 14:39:33 -0400

    "top" pages show in English if no language is specified
    See details of #0b28d985 for when the language is not specified.

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,12 +1,26 @@
 class TopController < ApplicationController
   def index
     @articles = Article.where("category = ?", "#{ArticlesController::CATEGORY_TOP}#index")
-    @articles = Article.search_by_language(@articles, session[:language])
+
+    # Show in english if no language is specified.
+    # This also applies for "all" languages.
+    if session[:language].present?
+      @articles = Article.search_by_language(@articles, session[:language])
+    else
+      @articles = Article.search_by_language(@articles, "English")
+    end
   end
 
   def about
     @title = "About"
     @articles = Article.where("category = ?", "#{ArticlesController::CATEGORY_TOP}#about")
-    @articles = Article.search_by_language(@articles, session[:language])
+
+    # Show in english if no language is specified.
+    # This also applies for "all" languages.
+    if session[:language].present?
+      @articles = Article.search_by_language(@articles, session[:language])
+    else
+      @articles = Article.search_by_language(@articles, "English")
+    end
   end
 end
