0b28d9853c2156d22e9a92c6f00b9e9ee8941abb
Naoki Mizuno <nigorojr@gmail.com>
2014-08-03 14:00:14 -0400

    Show language specific "top" pages if session[:language] exists
    The default page is shown if it the key doesn't exist. The key doesn't exist
    on first visit and when "All" is selected in the language selector.

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,10 +1,12 @@
 class TopController < ApplicationController
   def index
     @articles = Article.where("category = ?", "#{ArticlesController::CATEGORY_TOP}#index")
+    @articles = Article.search_by_language(@articles, session[:language])
   end
 
   def about
     @title = "About"
     @articles = Article.where("category = ?", "#{ArticlesController::CATEGORY_TOP}#about")
+    @articles = Article.search_by_language(@articles, session[:language])
   end
 end
